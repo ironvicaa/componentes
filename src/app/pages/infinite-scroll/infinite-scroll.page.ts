45958d0a774b377592309015ba867aeb5741b1d3
vic <vic231@gmail.com>
2022-03-26 19:16:20 -0600

    Actualizacion de archivos 26-03-2022

--- src/app/pages/infinite-scroll/infinite-scroll.page.ts
+++ src/app/pages/infinite-scroll/infinite-scroll.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-infinite-scroll',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfiniteScrollPage implements OnInit {
 
+  @ViewChild  ( IonInfiniteScroll ) infiniteScroll: IonInfiniteScroll
+
+  data: any[] = Array(20);
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loadData( event ) {
+    const data2 = Array(20);
+
+    setTimeout(() => {
+      this.data.push(...data2);
+      this.infiniteScroll.complete();
+
+      //Validar para que el infinitScroll se termine
+      if(this.data.length > 100) {
+        this.infiniteScroll.disabled = true;
+      }
+    }, 1000);
+
+    
   }
 
 }
